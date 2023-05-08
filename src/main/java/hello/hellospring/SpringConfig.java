40cd661deb43a3c23c7016876d513080a2fc8daf
CJ <choijun0627@konkuk.ac.kr>
2023-05-08 22:46:28 +0900

    <feat> 스프링 입문 프로젝트 section 6
    스프링 데이터 JPA
    
    스프링 데이터 JPA를 사용하기 위한 인터페이스 작성
    스프링 데이터 JPA를 사용하기 위한 SpringConfig 변경

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -5,23 +5,34 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import javax.persistence.EntityManager;
 import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
-    private final DataSource dataSource;
-    private final EntityManager em;
-    public SpringConfig(DataSource dataSource, EntityManager em) {
-        this.dataSource = dataSource;
-        this.em = em;
+    private final MemberRepository memberRepository;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
-    @Bean
-    public MemberRepository memberRepository() {
-// return new MemoryMemberRepository();
-// return new JdbcMemberRepository(dataSource);
-// return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+
+
+//    private final DataSource dataSource;
+//    private final EntityManager em;
+//    public SpringConfig(DataSource dataSource, EntityManager em) {
+//        this.dataSource = dataSource;
+//        this.em = em;
+//    }
+//    @Bean
+//    public MemberService memberService() {
+//        return new MemberService(memberRepository());
+//    }
+//    @Bean
+//    public MemberRepository memberRepository() {
+//    // return new MemoryMemberRepository();
+//    // return new JdbcMemberRepository(dataSource);
+//    // return new JdbcTemplateMemberRepository(dataSource);
+//    // return new JpaMemberRepository(em);
+//    }
 }
